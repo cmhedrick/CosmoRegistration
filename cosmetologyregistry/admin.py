bf7140e61a44dbb8bc451db8d2cfac601ae78d18
Chris Hedrick <chedrick@Karkat.(none)>
2013-01-18 14:31:28 -0500

    added static diectory w/ files from old static sit

---
+++ cosmetologyregistry/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from cosmetologyregistry.models import Consult, TextResponse, Choose, Service
+
+admin.site.register(Consult)
+admin.site.register(TextResponse)
+admin.site.register(Choose)
+admin.site.register(Service)
