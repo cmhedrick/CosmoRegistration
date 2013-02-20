4477a0e576521c8c81124ae5e34f4af3d15afe85
Chris Hedrick <chedrick@Karkat.(none)>
2013-02-20 15:06:40 -0500

    added context processor for next appointment

--- cosmetologyregistry/settings.py
+++ cosmetologyregistry/settings.py
@@ -3,3 +3,8 @@ import os
 # Import the proper settings_[environment].py file (defaults to dev)
 DJANGO_ENVIRONMENT = os.environ.get('DJANGO_ENVIRONMENT', 'dev')
 exec('from settings_%s import *' % DJANGO_ENVIRONMENT)
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'cosmetologyregistry.views.nextapt_context',
+)
--- cosmetologyregistry/views.py
+++ cosmetologyregistry/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render_to_response
+import datetime
 from models import Consult, TextResponse, Choose, Service
 
+def nextapt_context(request):
+    return {'nextapt': "January 3rd",}
