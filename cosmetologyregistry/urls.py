c5b7cfb98d4666ea1f724a121ed7ffc11fa030b1
Chris Hedrick <chedrick@Karkat.(none)>
2013-02-22 13:54:47 -0500

    userauth now enabled

--- cosmetologyregistry/urls.py
+++ cosmetologyregistry/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login, logout
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -6,4 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='homepage.html')),
+    url(r'^login/$', login),
+    url(r'^logout/$', logout, {'next_page': '/'}),
 )
