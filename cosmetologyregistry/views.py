4ca9a67ad8fb7c1349d0468fa03f9de8c52fa2a7
Chris Hedrick <chedrick@Karkat.(none)>
2013-02-28 14:21:15 -0500

    Fixed nxtapt logic to use database

--- cosmetologyregistry/views.py
+++ cosmetologyregistry/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render_to_response
 import datetime
-from models import Consult, TextResponse, Choose, Service
+from models import Consult, TextResponse, Choose, Service, Appointment
 
 def nextapt_context(request):
-    return {'nextapt': "January 3rd",}
+    apt = Appointment.objects.filter(
+        user=request.user
+    ).filter(
+        date_time__gte=datetime.datetime.now()
+    )
+    if apt:
+        dt = apt[0].date_time.strftime("%b %e %I:%M %p")
+    else:
+        dt = "no appointment"
+    return {'nextapt': dt,}
