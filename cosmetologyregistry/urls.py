ab1dd37c7294a5ca84a44c0628ca1f81d08881fd
Chris Hedrick <chedrick@Karkat.(none)>
2013-03-04 15:02:44 -0500

    got login to work

--- cosmetologyregistry/urls.py
+++ cosmetologyregistry/urls.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import login, logout
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -8,6 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='homepage.html')),
-    url(r'^login/$', login),
-    url(r'^logout/$', logout, {'next_page': '/'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html',}, ),
 )
