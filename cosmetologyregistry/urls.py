da8d1d23b4d53570063b514f2c1438b4ee5ea633
Chris Hedrick <chedrick@Karkat.(none)>
2013-03-26 15:54:27 -0400

    created login/register new user view

--- cosmetologyregistry/urls.py
+++ cosmetologyregistry/urls.py
@@ -2,14 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from views import MakeAptView, UpdateUserView
+from views import MakeAptView, UpdateUserView, LoginView
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='homepage.html')),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html',}, ),
+    url(r'^login/$', LoginView.as_view()),
     url(r'^visit_us/$', TemplateView.as_view(template_name='visit_us.html')),
     url(r'^price_list/$', TemplateView.as_view(template_name='price_list.html')),
     url(r'^curriculum/$', TemplateView.as_view(template_name='curriculum.html')),
--- cosmetologyregistry/views.py
+++ cosmetologyregistry/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render_to_response
 import datetime
 from dateutil import parser
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from models import UserProfile, Consult, TextResponse, Choose, Service, Appointment
 from django.views.generic import FormView, UpdateView
-from django.forms import ModelForm
+from django.forms import ModelForm, Form, CharField
+
+PASSWORD_MISMATCH = 'Username and Password do not match'
+USERNAME_TAKEN = 'Username is taken. Please choose another'
+PASSWORD_CONFIRM_MISMATCH = 'Password and Confirm Password do not match'
 
 def nextapt_context(request):
     if request.user.is_authenticated():
@@ -79,3 +84,68 @@ class UpdateUserView(UpdateView):
 
     def get_success_url(self):
         return '/'
+
+class LoginForm(Form):
+    new_first_name = CharField(label='Your first name')
+    new_last_name = CharField(label='Your last name')
+    new_username = CharField(label='Your username')
+    new_password = CharField(label='Your password')
+    new_password_confirm = CharField(label='Confirm password')
+    existing_username = CharField(label='Your username')
+    existing_password = CharField(label='Your password')
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        if 'register-button' in self.data:
+            if 'existing_username' in self.errors:
+                del self.errors['existing_username']
+            if 'existing_password' in self.errors:
+                del self.errors['existing_password']
+            if self.is_valid():
+                objlist = User.objects.filter(username=self.data['new_username'])
+                if objlist:
+                    self.errors['new_username'] = USERNAME_TAKEN
+                else:
+                    if self.data['new_password'] != self.data['new_password_confirm']:
+                        self.errors['new_password_confirm'] = PASSWORD_CONFIRM_MISMATCH
+        elif 'login-button' in self.data:
+            if 'new_first_name' in self.errors:
+                del self.errors['new_first_name']
+            if 'new_last_name' in self.errors:
+                del self.errors['new_last_name']
+            if 'new_username' in self.errors:
+                del self.errors['new_username']
+            if 'new_password' in self.errors:
+                del self.errors['new_password']
+            if 'new_password_confirm' in self.errors:
+                del self.errors['new_password_confirm']
+            if self.is_valid():
+                objlist = User.objects.filter(username=self.data['existing_username'])
+                if objlist:
+                    user = objlist[0]
+                    if user.check_password(self.data['existing_password']):
+                        return cleaned_data
+                self.errors['existing_username'] = PASSWORD_MISMATCH
+        return cleaned_data
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+
+    def get_success_url(self):
+        return '/' 
+
+    def form_valid(self, form):
+        if 'register-button' in form.data:
+            username = form.data['new_username']
+            password = form.data['new_password']
+            user = User.objects.create_user(username, password=password)
+            user.save()
+            user = authenticate(username=username, password=password)
+            login(self.request, user)
+        elif 'login-button' in form.data:
+            username = form.data['existing_username']
+            password = form.data['existing_password']
+            user = authenticate(username=username, password=password)
+            login(self.request, user)
+        return super(LoginView, self).form_valid(form)
